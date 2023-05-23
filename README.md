277221b8d7d3886fb7661241f245f49c959ec7c9
kim.sg <ksgn1514@gmail.com>
2023-05-23 23:00:46 +0900

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+■環境
+java　11.0.19
+springframework.boot version '2.7.12'
+thymeleaf
+
+mysql
+
---
+++ src/main/java/com/wvstudy/wvstudy/WvstudyApplication.java
@@ -0,0 +1,13 @@
+package com.wvstudy.wvstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WvstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WvstudyApplication.class, args);
+	}
+
+}
