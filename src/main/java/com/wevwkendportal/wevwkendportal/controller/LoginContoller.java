c0d79c8f75f93d47b3edd5a0e2e25e3918adcfda
sunyoung kim <lala673@naver.com>
2023-07-31 22:58:59 +0900

    test

--- src/main/java/com/wevwkendportal/wevwkendportal/controller/LoginContoller.java
+++ src/main/java/com/wevwkendportal/wevwkendportal/controller/LoginContoller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LoginContoller {
 
     @GetMapping("/")
-    public String index() {
+    public String dog() {
 
 		return "content/login";
 	}
